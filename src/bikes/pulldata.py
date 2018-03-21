133efcfaded2c42b411c57e96d59f2208be2709c
Mariyana Levova <mariyana.levova@ucdconnect.ie>
2018-03-21 17:17:36 +0000

    added station status to db

--- src/bikes/pulldata.py
+++ src/bikes/pulldata.py
@@ -1,20 +1,17 @@
-"""db = pymysql.connect(
-            host='34.209.36.30', # database IP
-            user='sqlpublic',
-            passwd='sqlpublic',
-            db='publicdb'
-        )"""
-
-
 def db_connect(query):
     import MySQLdb
     db = MySQLdb.connect("34.209.36.30", "sqlpublic", "sqlpublic", "publicdb") 
     curs=db.cursor() 
     curs.execute(query)
-    reading = curs.fetchall() 
+    reading = curs.fetchall()
     return reading
 
 query = "SELECT lng FROM bikes WHERE address = 'Grand Canal Dock';"
-print(db_connect(query))
+#print(db_connect(query))
 
+address = "SELECT distinct address,lat,lng FROM bikes;"
+db_connect(address)
+
+for i in db_connect(address):
+    print("Station is",i[0],"Latitude is",i[1],"Longitude is",i[2])
 
