ba7ce2e1dd718f924e150f7fd85708b738a95d87
Mariyana Levova <mariyana.levova@ucdconnect.ie>
2018-03-28 18:43:41 +0100

    working on html

--- testapp/dummyapp/app/__init__.py
+++ testapp/dummyapp/app/__init__.py
@@ -1,8 +1,4 @@
-
-
 from flask import Flask
-from flask_googlemaps import GoogleMaps
 app = Flask(__name__)
 from app import views
 
-GoogleMaps(app, key="AIzaSyBUgHP1nB5Nsm9k8IELrBRMpSSueJYA0r4")
--- testapp/dummyapp/run.py
+++ testapp/dummyapp/run.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from app import app
-if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":    
+    app.run(host='0.0.0.0', port=5000)
