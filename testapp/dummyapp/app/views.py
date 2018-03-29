ca9a18b56eea69f7a7b2738b79e33b778da494e1
ciaradillon16 <ciara.dillon1@ucdconnect.ie>
2018-03-29 21:46:29 +0100

    Marker bounce and change to SQL query

--- testapp/dummyapp/app/views.py
+++ testapp/dummyapp/app/views.py
@@ -5,14 +5,6 @@ import simplejson as json
 from pprint import pprint
 from app import app
 
-#turn my qery intot this format
-#pass with flask to html
-#our_json = "var data = {BarrowStreet:{lat:53.3417,lng:-6.2362},BensonStreet:{lat:53.3442,lng:-6.23345}}"
-
-"""def test_connection(self):
-    with app.app_context():
-        print(current_app.name)"""
-
 @app.route("/locs")
 def get_locs():
     #print("1")
@@ -20,7 +12,6 @@ def get_locs():
     connect = engine.connect()
     locs = []
     rows = connect.execute("SELECT address,lat,lng FROM bikes;")
-    #print("connected")
     for row in rows:
         locs.append(dict(row))
     return jsonify(locs=locs)
@@ -29,17 +20,14 @@ def db_connect():
     engine = create_engine("mysql+pymysql://publicdb:sqlpublic@52.43.48.163:3306/sqlpublic",echo=False)
     return engine
 
-#our_json = get_locs()
-
 @app.route('/')
 def index():
     engine = db_connect()
     connect = engine.connect()
     points = []
-    rows = connect.execute("SELECT address,lat,lng FROM static_bikes;")
-    #print("connected")
+    #rows = connect.execute("SELECT address,lat,lng FROM static_bikes;")
+    rows = connect.execute("SELECT distinct address,last_update,available_bike_stands,lat,lng,available_bikes, status, banking FROM bikes s1 WHERE last_update = (SELECT MAX(last_update) FROM bikes s2 WHERE s1.address = s2.address)GROUP BY address;")
     for row in rows:
         points.append(dict(row))
     jsonify(points=points)
     return render_template("index.html", data=points)
-
