239af554d4c48b8c50df72e09c55cda1f1f4036b
Mariyana Levova <mariyana.levova@ucdconnect.ie>
2018-04-15 21:41:15 +0100

    updated weather scraper to unclude forecast data

--- src/bikes/weather_scraper.py
+++ src/bikes/weather_scraper.py
@@ -29,12 +29,11 @@ def insertDb(data, db):
         cursor = db.cursor()
         
         add_weather = ("REPLACE INTO weather "
-                    "(id, main, description, temp, icon, pressure, humidity, temp_min, temp_max, visibility, windspeed) "
-                    "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
+                    "(id, main, description, temp, icon, pressure, humidity, temp_min, temp_max, windspeed, threeH_rain_predict, future_icon ) "
+                    "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
         
         cursor.execute(add_weather, data)
         db.commit()
-        
         
     except Exception as e: 
         template = "Insert An exception of type {0} occurred. Arguments:\n{1!r}"
@@ -43,30 +42,36 @@ def insertDb(data, db):
 
 def main(): 
 
-    url = "http://api.openweathermap.org/data/2.5/weather?q=dublin,ie&units=metric&appid=a87a4c45fc8819c6fd6dae5a0db2439a"
+    url = "http://api.openweathermap.org/data/2.5/forecast?q=dublin,ie&units=metric&appid=a87a4c45fc8819c6fd6dae5a0db2439a"
     db = dbConnect()
     print("Connected!")
     while True: 
         rawData = requests.get(url)
         print(rawData.status_code)
+
         if rawData.status_code == 200:
             data = json.loads(rawData.text)
             print("Working")
-            id = data['sys']['country']
+            id = data['city']['country']
             print(id)
-            main = data['weather'][0]['main']
-            desc = data['weather'][0]['description']
-            temp = data['main']['temp']
-            icon = data['weather'][0]['icon']
-            pressure = data['main']['pressure']
-            humidity = data['main']['humidity']
-            temp_min = data['main']['temp_min']
-            temp_max = data['main']['temp_max']
-            visibility = data['visibility']
-            windspeed = data['wind']['speed']
+
+            main = data['list'][0]['weather'][0]['main']
+            desc = data['list'][0]['weather'][0]['description']
+            temp = data['list'][0]['main']['temp']
+            icon = data['list'][0]['weather'][0]['icon']
+            pressure = data['list'][0]['main']['pressure']
+            humidity = data['list'][0]['main']['humidity']
+            temp_min = data['list'][0]['main']['temp_min']
+            temp_max = data['list'][0]['main']['temp_max']
+            windspeed = data['list'][0]['wind']['speed']
+            threeH_rain_predict = data['list'][2]['rain']['3h']
+            future_icon = data['list'][2]['weather'][0]['icon']
             
-            data = [id, main, desc, temp, icon, pressure, humidity, temp_min, temp_max, visibility, windspeed]
+            data = [id, main, desc, temp, icon, pressure, humidity, temp_min, temp_max, windspeed, threeH_rain_predict, future_icon]
             insertDb(data, db)
+
+        
+        print('sleeping') 
         time.sleep(1800)
            
 if __name__ == "__main__":
